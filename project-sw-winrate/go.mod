2eaeed5df453b9b90e9a011618a06ce4e9de3d14
Diogo L Andrade <127452332+soothsayerdev@users.noreply.github.com>
2024-09-24 16:24:01 -0300

    Update/Create Test Env

--- project-sw-winrate/go.mod
+++ project-sw-winrate/go.mod
@@ -1,8 +1,10 @@
-module project-sw-winrate
+module github.com/soothsayerdev/WinRate
 
 go 1.23.1
 
 require (
-    github.com/gorilla/mux v1.8.1 // indirect
-    github.com/go-sql-driver/mysql v1.6.0    
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/gorilla/mux v1.8.1
 )
+
+require golang.org/x/crypto v0.27.0
--- project-sw-winrate/project2.go
+++ project-sw-winrate/project2.go
@@ -7,12 +7,13 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"golang.org/x/crypto/bcrypt" // To compare password with hash
 
-	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	_ "github.com/go-sql-driver/mysql"
 )
 
-var db *sql.db
+var db *sql.DB
 
 func main() {
 	var err error
@@ -42,7 +43,7 @@ func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/register", registerUser).Methods("POST")
-	router.HandleFunc("login", loginUser).Methods("POST")
+	router.HandleFunc("/login", loginUser).Methods("POST")
 	router.HandleFunc("/decks", createDeck).Methods("POST")
 	router.HandleFunc("matches", createMatch).Methods("POST")
 	router.HandleFunc("/matches/{id}", updateMatch).Methods("PUT")
@@ -51,12 +52,18 @@ func main() {
 }
 
 type User struct {
-	ID int `json:"id"`
-	Email string `json:"email"`
-	Password string `json:"password"`
+	ID 			int `json:"id"`
+	Email 		string `json:"email"`
+	Password 	string `json:"password"`
 }
 
 type Deck struct {
+	ID 					int `json:"id"`
+	UserID 				int `json:"user_id"`
+	DeckName 	 		string `json:"deck_name"`
+}
+
+type Match struct {
 	ID 					int `json:"id"`
 	UserDeckID 			int `json:"user_deck_id"`
 	OpponentDeckID 		int `json:"opponent_deck_id"`
@@ -66,21 +73,95 @@ type Deck struct {
 
 
 func registerUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
 	var user User
+
+	// Decodify JSON requested to struct User
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, "Dados invalidos", http.StatusBadRequest)
 		return
 	}
 
-	_, err = db.Exec("INSERT INTO user(email, password) VALUES (?, ?)", user.Email, user.Password)
+	// Check if email already exists in database
+	var existingUserID int
+	err = db.QueryRow("SELECT id FROM users WHERE email = ?", user.Email).Scan(&existingUserID)
+	if err == nil { // If email exists, return an error message
+        http.Error(w, "Email já cadastrado", http.StatusConflict)
+        return
+    }
+	
+	// Generate the hash of password
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
 	if err != nil {
-		http.Error(w, err.Error)
+		log.Fatal(err)
+		http.Error(w, "Erro ao gerar senha", http.StatusInternalServerError)
 		return
 	}
 	
-	fmt.Fprintf(w, "Usario registrado com sucesso!")
+    // Insert new user into database
+	query := "INSERT INTO users (email, password) VALUES (?, ?)"
+	_, err = db.Exec(query, user.Email, string(hashedPassword))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	
+	// Return the response of sucess in register
+	response := map[string]string{
+		"message": "Usuario registrado com sucesso!",
+	}
+	json.NewEncoder(w).Encode(response)
+	// fmt.Fprintf(w, "Usario registrado com sucesso!")
 }
+
+// Request email and password of user
+// Verify existente of user with this email in database
+// Compare to password with the password in database
+// Return response of sucess/fail of login
+func loginUser(w http.ResponseWriter, r *http.Request) {
+	// define type of response JSON
+	w.Header().Set("Content-Type", "application/json")
+
+	var user User
+	var dbPassword string
+
+	// decodify JSON requested in requisition to struct User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, "Dados invalidos", http.StatusBadRequest)
+		return
+	}
+
+	// consult in database to verify if user exists
+	query := "SELECT id, password FROM users WHERE email = ?"
+	err = db.QueryRow(query, user.Email).Scan(&user.ID, &dbPassword)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			http.Error(w, "Usuario não encontrado", http.StatusUnauthorized)
+		} else {
+			http.Error(w, "Erro ao buscar o usuario", http.StatusInternalServerError)
+		}
+		return
+	}
+
+	// Verify if password provided matches to hash in database
+	err = bcrypt.CompareHashAndPassword([]byte(dbPassword), []byte(user.Password))
+	if err != nil {
+		http.Error(w, "Senha incorreta", http.StatusUnauthorized)
+		return
+	}
+
+	// Return a response of login sucess
+	response := map[string]interface{}{
+		"message": "Login realizado com sucesso",
+		"user_id": user.ID,
+	}
+	json.NewEncoder(w).Encode(response)
+}
+
+
 
 func createDeck(w http.ResponseWriter, r *http.Request) {
 	var deck Deck
@@ -90,7 +171,7 @@ func createDeck(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	_, err = db.Exec("INSERT INTO decks (user_id, deck_name) VALUES (?, ?)", deck.UserDeckID, deck.DeckName)
+	_, err = db.Exec("INSERT INTO decks (user_id, deck_name) VALUES (?, ?)", deck.UserID, deck.DeckName)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -118,7 +199,7 @@ func createMatch(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func updateMatch(w, http.ResponseWriter, r *http.Request) {
+func updateMatch(w http.ResponseWriter, r *http.Request) {
 	var match Match
 	err := json.NewDecoder(r.Body).Decode(&match)
 	if err != nil {
